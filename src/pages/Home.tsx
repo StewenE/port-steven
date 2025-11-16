6a0008d21578935a1f6ae155a6a75d84fb1000b1
sterni <sterni@taltech.ee>
2025-11-16 16:30:09 +0200

    feat: implement example NavBar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             <ThemeToggle />
-            Hello
             <NavBar />
         </div>
     );
